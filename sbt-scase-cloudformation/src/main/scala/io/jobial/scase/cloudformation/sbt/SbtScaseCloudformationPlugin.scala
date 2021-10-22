7ed451ddd8018a9e7d4658714437938533fcc301
orbang <orbang@jobial.io>
2021-10-22 02:40:20 +0100

    Added some notes on the sbt api.

--- sbt-scase-cloudformation/src/main/scala/io/jobial/scase/cloudformation/sbt/SbtScaseCloudformationPlugin.scala
+++ sbt-scase-cloudformation/src/main/scala/io/jobial/scase/cloudformation/sbt/SbtScaseCloudformationPlugin.scala
@@ -6,9 +6,18 @@ import Keys._
 import sbt.State.stateOps
 import complete.DefaultParsers._
 
-// Loosely based on https://codewithstyle.info/how-to-build-a-simple-sbt-plugin/
-// https://stackoverflow.com/questions/8973666/how-to-access-a-sbt-projects-settings-in-a-plugin
-
+/**
+ * Loosely based on
+ * 
+ * https://codewithstyle.info/how-to-build-a-simple-sbt-plugin/
+ * https://stackoverflow.com/questions/8973666/how-to-access-a-sbt-projects-settings-in-a-plugin
+ * https://stackoverflow.com/questions/22372717/sbt-how-to-use-classes-from-build-sbt-inside-plugin-task-execution
+ * https://stackoverflow.com/questions/37369884/sbt-how-to-refer-to-other-project-source-code-in-build-sbt
+ * https://stackoverflow.com/questions/23409993/defining-sbt-task-that-invokes-method-from-project-code
+ * 
+ * sbt compile publishLocal
+ * sbt "scaseCloudformation 1 2 3"
+ */
 object SbtScaseCloudformationPlugin extends AutoPlugin {
   override def trigger = allRequirements
   //override lazy val buildSettings = Seq(commands += helloCommand)
@@ -29,7 +38,10 @@ object SbtScaseCloudformationPlugin extends AutoPlugin {
       val args = spaceDelimited("").parsed
       
       //println("cloudformationStackClass: " + cloudformationStackClass.value)
-      println(s"scaseCloudformation called with args ${args.toList} for " + cloudformationStackClass.value)
+      if (cloudformationStackClass.value != "") {
+        println(s"scaseCloudformation called with args ${args.toList} for " + cloudformationStackClass.value)
+        println(Class.forName(cloudformationStackClass.value))
+      }
     }
   )
 
