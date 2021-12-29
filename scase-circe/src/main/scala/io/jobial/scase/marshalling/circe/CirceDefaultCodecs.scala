690e90f1a4190d4ad66b97b154e90b058b68a0af
orbang <orbang@jobial.io>
2021-12-29 22:00:33 +0000

    Implemented either marshalling, Circe default codec for Throwable

--- scase-circe/src/main/scala/io/jobial/scase/marshalling/circe/CirceDefaultCodecs.scala
+++ scase-circe/src/main/scala/io/jobial/scase/marshalling/circe/CirceDefaultCodecs.scala
@@ -1,10 +1,27 @@
 package io.jobial.scase.marshalling.circe
 
-import io.circe.{Decoder, Encoder}
+import io.circe.Decoder.Result
+import io.circe.{Decoder, Encoder, HCursor, Json}
 
 trait CirceDefaultCodecs {
 
-  implicit val throwableEncoder: Encoder[Throwable] = ???
+  implicit val throwableEncoder = new Encoder[Throwable] {
+    override def apply(a: Throwable): Json = Json.obj(
+      "error" -> Json.obj(
+        "message" -> Json.fromString(a.getMessage),
+        "type" -> Json.fromString(a.getClass.getName)
+      )
+    )
+  }
 
-  implicit val throwableDecoder: Decoder[Throwable] = ???
+  implicit val throwableDecoder = new Decoder[Throwable] {
+    override def apply(c: HCursor): Result[Throwable] = {
+      val e = c.downField("error")
+      for {
+        message <- e.downField("message").as[String]
+        klass <- e.downField("type").as[String]
+      } yield
+        Class.forName(klass).getConstructor(classOf[String]).newInstance(message).asInstanceOf[Throwable]
+    }
+  }
 }
--- scase-core/src/main/scala/io/jobial/scase/marshalling/DefaultMarshallers.scala
+++ scase-core/src/main/scala/io/jobial/scase/marshalling/DefaultMarshallers.scala
@@ -1,4 +1,5 @@
 package io.jobial.scase.marshalling
+
 import cats.effect.IO
 
 import java.io.{InputStream, OutputStream}
@@ -7,19 +8,58 @@ trait DefaultMarshallers {
 
   implicit def eitherMarshaller[A: Marshaller, B: Marshaller]: Marshaller[Either[A, B]] =
     new Marshaller[Either[A, B]] {
-      override def marshal(o: Either[A, B]): Array[Byte] = ???
+      override def marshal(o: Either[A, B]): Array[Byte] = o match {
+        case Left(a) =>
+          Marshaller[A].marshal(a)
+        case Right(b) =>
+          Marshaller[B].marshal(b)
+      }
 
-      override def marshal(o: Either[A, B], out: OutputStream): IO[OutputStream] = ???
+      override def marshal(o: Either[A, B], out: OutputStream): IO[OutputStream] = o match {
+        case Left(a) =>
+          Marshaller[A].marshal(a, out)
+        case Right(b) =>
+          Marshaller[B].marshal(b, out)
+      }
 
-      override def marshalToText(o: Either[A, B]): String = ???
+      override def marshalToText(o: Either[A, B]): String = o match {
+        case Left(a) =>
+          Marshaller[A].marshalToText(a)
+        case Right(b) =>
+          Marshaller[B].marshalToText(b)
+      }
     }
-    
+
   implicit def eitherUnmarshaller[A: Unmarshaller, B: Unmarshaller]: Unmarshaller[Either[A, B]] =
     new Unmarshaller[Either[A, B]] {
-      override def unmarshal(bytes: Array[Byte]): Either[Throwable, Either[A, B]] = ???
+      override def unmarshal(bytes: Array[Byte]): Either[Throwable, Either[A, B]] =
+        Unmarshaller[B].unmarshal(bytes) match {
+          case Left(_) =>
+            Unmarshaller[A].unmarshal(bytes) match {
+              case Left(l) =>
+                Left(l)
+              case Right(r) =>
+                Right[Throwable, Either[A, B]](Left(r))
+            }
+          case Right(r) =>
+            Right[Throwable, Either[A, B]](Right(r))
+        }
 
-      override def unmarshal(in: InputStream): IO[Either[A, B]] = ???
+      override def unmarshal(in: InputStream): IO[Either[A, B]] =
+        Unmarshaller[B].unmarshal(in).map(b => Right[A, B](b)).handleErrorWith(_ =>
+          Unmarshaller[A].unmarshal(in).map(a => Left[A, B](a)))
 
-      override def unmarshalFromText(text: String): Either[Throwable, Either[A, B]] = ???
+      override def unmarshalFromText(text: String): Either[Throwable, Either[A, B]] =
+        Unmarshaller[B].unmarshalFromText(text) match {
+          case Left(_) =>
+            Unmarshaller[A].unmarshalFromText(text) match {
+              case Left(l) =>
+                Left(l)
+              case Right(r) =>
+                Right[Throwable, Either[A, B]](Left(r))
+            }
+          case Right(r) =>
+            Right[Throwable, Either[A, B]](Right(r))
+        }
     }
 }
