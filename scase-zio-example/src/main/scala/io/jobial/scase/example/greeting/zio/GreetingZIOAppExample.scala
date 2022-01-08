1bf0989928087bb60bebde78891c5e47c65b12f9
orbang <orbang@jobial.io>
2022-01-08 21:32:51 +0000

    Renamed ZIO example app.

--- scase-zio-example/src/main/scala/io/jobial/scase/example/greeting/zio/GreetingZIOAppExample.scala
+++ scase-zio-example/src/main/scala/io/jobial/scase/example/greeting/zio/GreetingZIOAppExample.scala
@@ -12,19 +12,20 @@
  */
 package io.jobial.scase.example.greeting.zio
 
+import io.jobial.sclap.zio.ZIOCommandLineApp
 import zio._
 import zio.interop.catz._
-import zio.interop.catz.taskEffectInstance
 import zio.interop.catz.implicits._
+import zio.console._
 
-// TODO: add this back when ZIO is upgraded and ExitCode is available...
-//object GreetingZIOAppExample extends zio.App with GreetingServiceConfig {
-//
-//  def run(args: List[String]) = 
-//    (for {
-//      t <- greetingServiceConfig.serviceAndClient[Task](new GreetingService {})
-//      (service, client) = t
-//      _ <- service.start
-//      helloResponse <- client ? Hello("world")
-//    } yield println(helloResponse.sayingHello)).exitCode
-//}
+object GreetingZIOAppExample extends ZIOCommandLineApp with GreetingServiceConfig {
+
+  def run =
+    for {
+      t <- greetingServiceConfig.serviceAndClient[Task](new GreetingService {})
+      (service, client) = t
+      _ <- service.start
+      helloResponse <- client ? Hello("world")
+      _ <- putStr(helloResponse.sayingHello)
+    } yield ()
+}
