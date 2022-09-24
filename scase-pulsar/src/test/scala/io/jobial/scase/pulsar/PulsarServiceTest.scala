93f11968c4f0aad6818dff5044123040c3c723ef
orbang <orbang@jobial.io>
2022-09-24 23:11:53 +0100

    Added remaining service config types for Pulsar

--- scase-pulsar/src/test/scala/io/jobial/scase/pulsar/PulsarServiceTest.scala
+++ scase-pulsar/src/test/scala/io/jobial/scase/pulsar/PulsarServiceTest.scala
@@ -1,10 +1,15 @@
 package io.jobial.scase.pulsar
 
 import cats.effect.IO
+import cats.effect.concurrent.Deferred
 import io.circe.generic.auto._
 import io.jobial.scase.core._
 import io.jobial.scase.marshalling.circe._
+import io.jobial.scase.pulsar.PulsarServiceConfiguration.destination
+import io.jobial.scase.pulsar.PulsarServiceConfiguration.handler
 import io.jobial.scase.pulsar.PulsarServiceConfiguration.requestResponse
+import io.jobial.scase.pulsar.PulsarServiceConfiguration.source
+import io.jobial.scase.pulsar.PulsarServiceConfiguration.stream
 import io.jobial.scase.util.Hash.uuid
 
 
@@ -53,4 +58,41 @@ class PulsarServiceTest
     } yield r
   }
 
+  "stream service" should "reply with error" in {
+    val responseTopic = s"hello-error-test-response-${uuid(5)}"
+    val serviceConfig = stream[TestRequest[_ <: TestResponse], TestResponse](
+      s"hello-error-test-${uuid(5)}", responseTopic)
+    val sourceConfig = source[TestResponse](responseTopic)
+
+    for {
+      service <- serviceConfig.service(requestHandlerWithError)
+      senderClient <- serviceConfig.client[IO]
+      receiverClient <- sourceConfig.client[IO]
+      r <- testStreamErrorReply(service, senderClient, receiverClient)
+    } yield r
+  }
+
+  "message handler service" should "receive successfully" in {
+    val serviceConfig = handler[TestRequest[_ <: TestResponse]](
+      s"hello-test-handler-${uuid(5)}")
+
+    for {
+      receivedMessage <- Deferred[IO, TestRequest[_ <: TestResponse]]
+      service <- serviceConfig.service(TestMessageHandler(receivedMessage))
+      senderClient <- serviceConfig.client[IO]
+      r <- testSuccessfulMessageHandlerReceive(service, senderClient, receivedMessage)
+    } yield r
+  }
+
+  "message destination" should "receive successfully" in {
+    val topic = s"hello-source-${uuid(5)}"
+    val destinationConfig = destination[TestRequest[_ <: TestResponse]](topic)
+    val sourceConfig = source[TestRequest[_ <: TestResponse]](topic)
+
+    for {
+      senderClient <- destinationConfig.client[IO]
+      receiverClient <- sourceConfig.client[IO]
+      r <- testMessageSourceReceive(senderClient, receiverClient)
+    } yield r
+  }
 }
