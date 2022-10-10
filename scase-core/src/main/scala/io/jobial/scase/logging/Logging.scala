99b441ebb5249bb59fe208422bcad54a3ae41484
orbang <orbang@jobial.io>
2022-10-10 20:32:05 +0100

    Added missing logging functions

--- scase-core/src/main/scala/io/jobial/scase/logging/Logging.scala
+++ scase-core/src/main/scala/io/jobial/scase/logging/Logging.scala
@@ -8,11 +8,19 @@ trait Logging extends LazyLogging {
 
   def trace[F[_] : Sync](msg: => String) = Sync[F].delay(logger.trace(msg))
 
+  def trace[F[_] : Sync](msg: => String, t: Throwable) = Sync[F].delay(logger.trace(msg, t))
+
   def debug[F[_] : Sync](msg: => String) = Sync[F].delay(logger.debug(msg))
 
+  def debug[F[_] : Sync](msg: => String, t: Throwable) = Sync[F].delay(logger.debug(msg, t))
+  
   def info[F[_] : Sync](msg: => String) = Sync[F].delay(logger.info(msg))
 
+  def info[F[_] : Sync](msg: => String, t: Throwable) = Sync[F].delay(logger.info(msg, t))
+
   def warn[F[_] : Sync](msg: => String) = Sync[F].delay(logger.warn(msg))
+
+  def warn[F[_] : Sync](msg: => String, t: Throwable) = Sync[F].delay(logger.warn(msg, t))
 
   def error[F[_] : Sync](msg: => String) = Sync[F].delay(logger.error(msg))
 
