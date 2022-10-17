94011b348fb37cd4bd0adf18277c5aa6b2d15c53
orbang <orbang@jobial.io>
2022-10-17 10:45:06 +0100

    Added builder functions to PulsarContext JavaDSL

--- scase-pulsar/src/main/java/io/jobial/scase/pulsar/javadsl/PulsarContext.java
+++ scase-pulsar/src/main/java/io/jobial/scase/pulsar/javadsl/PulsarContext.java
@@ -7,7 +7,31 @@ public class PulsarContext {
     public PulsarContext() {
     }
 
+    public PulsarContext(io.jobial.scase.pulsar.PulsarContext context) {
+        this.context = context;
+    }
+
     public io.jobial.scase.pulsar.PulsarContext getContext() {
         return context;
+    }
+    
+    public PulsarContext withHost(String host) {
+        return new PulsarContext(context.copy(host, context.port(), context.tenant(), context.namespace(), context.useDaemonThreadsInClient()));
+    }
+
+    public PulsarContext withPort(int port) {
+        return new PulsarContext(context.copy(context.host(), port, context.tenant(), context.namespace(), context.useDaemonThreadsInClient()));
+    }
+
+    public PulsarContext withTenant(String tenant) {
+        return new PulsarContext(context.copy(context.host(), context.port(), tenant, context.namespace(), context.useDaemonThreadsInClient()));
+    }
+
+    public PulsarContext withNamespace(String namespace) {
+        return new PulsarContext(context.copy(context.host(), context.port(), context.tenant(), namespace, context.useDaemonThreadsInClient()));
+    }
+
+    public PulsarContext withUseDaemonThreadsInClient(Boolean useDaemonThreadsInClient) {
+        return new PulsarContext(context.copy(context.host(), context.port(), context.tenant(), context.namespace(), useDaemonThreadsInClient));
     }
 }
