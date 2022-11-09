dccee412b9d18913b65529350d2230b73bcf5d5e
orbang <orbang@jobial.io>
2022-11-09 03:07:35 +0000

    Separated one-way and request-response cases in bridge; improved timeout support; fixed Pulsar one-way forwarding

--- scase-tibco-rv/src/main/scala/io/jobial/scase/tibrv/TibrvSupport.scala
+++ scase-tibco-rv/src/main/scala/io/jobial/scase/tibrv/TibrvSupport.scala
@@ -8,8 +8,16 @@ trait TibrvSupport {
   val context: TibrvContext
   
   def initRv = if (!Tibrv.isValid) Tibrv.open(Tibrv.IMPL_NATIVE)
-  
+
+  val networkWithSemicolon = for {
+    network <- context.network
+  } yield
+    if (network.startsWith(";"))
+      network
+    else
+      s";$network"
+      
   def createTransport =
-    new TibrvRvdTransport(context.service.getOrElse(null), context.network.getOrElse(null), s"${context.host}:${context.port}")
+    new TibrvRvdTransport(context.service.getOrElse(null), networkWithSemicolon.getOrElse(null), s"${context.host}:${context.port}")
 
 }
