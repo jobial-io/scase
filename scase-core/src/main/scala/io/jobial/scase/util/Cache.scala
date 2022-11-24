1e988684d0d30c0f4fdf2e8a6c3db3cf48ca5203
orbang <orbang@jobial.io>
2022-11-24 18:32:40 +0000

    Improved cching of clients in bridge; stop clients after some timeout

--- scase-core/src/main/scala/io/jobial/scase/util/Cache.scala
+++ scase-core/src/main/scala/io/jobial/scase/util/Cache.scala
@@ -4,29 +4,48 @@ import cats.effect.Concurrent
 import cats.effect.Concurrent.memoize
 import cats.effect.concurrent.Ref
 import cats.implicits._
+import io.jobial.scase.core.impl.CatsUtils
+import java.lang.System.currentTimeMillis
+import scala.concurrent.duration.Duration
 
-class Cache[F[_] : Concurrent, A, B](store: Ref[F, Map[A, F[B]]]) {
+class Cache[F[_] : Concurrent, A, B](store: Ref[F, Map[A, CacheEntry[F, A, B]]], timeout: Duration,
+  accessCount: Ref[F, Long]) extends CatsUtils {
 
-  def getOrCreate(key: A, value: F[B]): F[B] =
+  def getOrCreate(key: A, value: F[B], finalizer: (A, B) => F[Unit] = { (_: A, _: B) => unit[F] }): F[B] =
     for {
       mv <- memoize(value)
       v <- store.modify(s => {
         s.get(key) match {
-          case Some(v) =>
-            (s, v)
+          case Some(entry) =>
+            (s, entry.value)
           case None =>
-            (s + (key -> mv), mv)
+            (s + (key -> CacheEntry(mv, currentTimeMillis, finalizer)), mv)
         }
       })
       v <- v
     } yield v
+
+  def cleanup =
+    store.modify { s =>
+      s.filter { case (key, entry) =>
+        currentTimeMillis - entry.timestamp < timeout.toMillis
+      } -> ()
+    }
+
 }
+
+case class CacheEntry[F[_], A, B](
+  value: F[B],
+  timestamp: Long,
+  finalizer: (A, B) => F[Unit]
+)
 
 object Cache {
 
-  def apply[F[_] : Concurrent, A, B] = {
+  def apply[F[_] : Concurrent, A, B](timeout: Duration = Duration.Inf) = {
     for {
-      store <- Ref.of[F, Map[A, F[B]]](Map())
-    } yield new Cache[F, A, B](store)
+      store <- Ref.of[F, Map[A, CacheEntry[F, A, B]]](Map())
+      accessCount <- Ref.of[F, Long](0)
+    } yield new Cache[F, A, B](store, timeout, accessCount)
   }
 }
