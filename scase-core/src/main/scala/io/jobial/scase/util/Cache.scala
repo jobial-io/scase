2d9eb095102a3982608da7fa0e470dbd64eb1fd9
orbang <orbang@jobial.io>
2022-11-24 20:10:06 +0000

    Fixed cache expiry; added test

--- scase-core/src/main/scala/io/jobial/scase/util/Cache.scala
+++ scase-core/src/main/scala/io/jobial/scase/util/Cache.scala
@@ -9,43 +9,59 @@ import java.lang.System.currentTimeMillis
 import scala.concurrent.duration.Duration
 
 class Cache[F[_] : Concurrent, A, B](store: Ref[F, Map[A, CacheEntry[F, A, B]]], timeout: Duration,
-  accessCount: Ref[F, Long]) extends CatsUtils {
+  accessCount: Ref[F, Long], cleanupFrequency: Int) extends CatsUtils {
 
-  def getOrCreate(key: A, value: F[B], finalizer: (A, B) => F[Unit] = { (_: A, _: B) => unit[F] }): F[B] =
+  def getOrCreate(key: A, value: F[B], onExpiry: (A, B) => F[Unit] = { (_: A, _: B) => unit[F] }): F[B] =
     for {
+      accessCount <- accessCount.modify(c => (c + 1, c + 1))
+      _ <- whenA(accessCount % cleanupFrequency == 0)(cleanup)
       mv <- memoize(value)
       v <- store.modify(s => {
         s.get(key) match {
           case Some(entry) =>
             (s, entry.value)
           case None =>
-            (s + (key -> CacheEntry(mv, currentTimeMillis, finalizer)), mv)
+            (s + (key -> CacheEntry(mv, currentTimeMillis, onExpiry)), mv)
         }
       })
       v <- v
     } yield v
 
   def cleanup =
-    store.modify { s =>
-      s.filter { case (key, entry) =>
-        currentTimeMillis - entry.timestamp < timeout.toMillis
-      } -> ()
-    }
+    for {
+      expired <- store.modify { s =>
+        s.partition { case (key, entry) =>
+          currentTimeMillis - entry.timestamp < timeout.toMillis
+        }
+      }
+      _ <- {
+        for {
+          (key, entry) <- expired.toList
+        } yield for {
+          value <- entry.value
+          r <- entry.onExpiry(key, value)
+        } yield r
+      }.sequence
+    } yield ()
 
+  def size =
+    for {
+      s <- store.get
+    } yield s.size
 }
 
 case class CacheEntry[F[_], A, B](
   value: F[B],
   timestamp: Long,
-  finalizer: (A, B) => F[Unit]
+  onExpiry: (A, B) => F[Unit]
 )
 
 object Cache {
 
-  def apply[F[_] : Concurrent, A, B](timeout: Duration = Duration.Inf) = {
+  def apply[F[_] : Concurrent, A, B](timeout: Duration = Duration.Inf, cleanupFrequency: Int = 100) = {
     for {
       store <- Ref.of[F, Map[A, CacheEntry[F, A, B]]](Map())
       accessCount <- Ref.of[F, Long](0)
-    } yield new Cache[F, A, B](store, timeout, accessCount)
+    } yield new Cache[F, A, B](store, timeout, accessCount, cleanupFrequency)
   }
 }
