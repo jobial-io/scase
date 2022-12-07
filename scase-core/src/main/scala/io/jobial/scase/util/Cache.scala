4bece88c961fc07ace40d097e40b9fcd18a89a6e
orbang <orbang@jobial.io>
2022-12-07 14:23:11 +0000

    Fixed timestamp update in cache

--- scase-core/src/main/scala/io/jobial/scase/util/Cache.scala
+++ scase-core/src/main/scala/io/jobial/scase/util/Cache.scala
@@ -14,17 +14,17 @@ class Cache[F[_] : Concurrent, A, B](store: Ref[F, Map[A, CacheEntry[F, A, B]]],
   def getOrCreate(key: A, value: F[B], onExpiry: (A, B) => F[Unit] = { (_: A, _: B) => unit[F] }): F[B] =
     for {
       accessCount <- accessCount.modify(c => (c + 1, c + 1))
-      _ <- whenA(accessCount % cleanupFrequency == 0)(cleanup)
       mv <- memoize(value)
       v <- store.modify(s => {
         s.get(key) match {
           case Some(entry) =>
-            (s, entry.value)
+            (s + (key -> entry.copy(timestamp = currentTimeMillis)), entry.value)
           case None =>
             (s + (key -> CacheEntry(mv, currentTimeMillis, onExpiry)), mv)
         }
       })
       v <- v
+      _ <- whenA(accessCount % cleanupFrequency == 0)(cleanup)
     } yield v
 
   def cleanup =
