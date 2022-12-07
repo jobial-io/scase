44e0f50d987f95da969229ce5fefd5e403b98e97
orbang <orbang@jobial.io>
2022-12-07 20:05:00 +0000

    Log statistics in bridge

--- scase-core/src/main/scala/io/jobial/scase/core/impl/ForwarderBridge.scala
+++ scase-core/src/main/scala/io/jobial/scase/core/impl/ForwarderBridge.scala
@@ -9,14 +9,16 @@ import io.jobial.scase.core._
 import io.jobial.scase.logging.Logging
 import io.jobial.scase.marshalling.Marshaller
 import io.jobial.scase.marshalling.Unmarshaller
-
 import scala.concurrent.duration.DurationInt
 
 class ForwarderBridge[F[_] : Concurrent : Timer, REQ: Unmarshaller, RESP: Marshaller](
   source: ReceiverClient[F, REQ],
   destination: MessageReceiveResult[F, RESP] => F[Option[MessageSendResult[F, RESP]]],
   filter: MessageReceiveResult[F, REQ] => F[Option[MessageReceiveResult[F, RESP]]],
-  stopped: Ref[F, Boolean]
+  val stopped: Ref[F, Boolean],
+  messageCounter: Ref[F, Long],
+  errorCounter: Ref[F, Long],
+  filteredMessageCounter: Ref[F, Long]
 ) extends DefaultService[F] with CatsUtils with Logging {
 
   def continueForwarding =
@@ -28,30 +30,43 @@ class ForwarderBridge[F[_] : Concurrent : Timer, REQ: Unmarshaller, RESP: Marsha
   def forward: F[Unit] =
     (for {
       receiveResult <- source.receiveWithContext(1.second)
+      _ <- messageCounter.update(_ + 1)
       filteredReceiveResult <- filter(receiveResult)
       sendResult <- filteredReceiveResult match {
         case Some(filteredReceiveResult) =>
           destination(filteredReceiveResult)
         case None =>
-          pure(None)
+          filteredMessageCounter.update(_ + 1) >>
+            pure(None)
       }
       r <- continueForwarding
     } yield r) handleErrorWith {
       case t: ReceiveTimeout =>
         continueForwarding
       case t: Throwable =>
-        error(s"error while forwarding in $this", t) >>
+        errorCounter.update(_ + 1) >>
+          error(s"error while forwarding in $this", t) >>
           continueForwarding
     }
 
-  def start: F[ServiceState[F]] =
-    start(forward) >> pure(new ServiceState[F] {
-      def stop = stopped.set(true) >> pure(this)
+  def start =
+    start(forward) >> pure(new ForwarderBridgeServiceState(this))
 
-      def join: F[ServiceState[F]] = waitFor(stopped.get)(pure(_)) >> pure(this)
+  def messageCount = messageCounter.get
 
-      def service = ForwarderBridge.this
-    })
+  def errorCount = errorCounter.get
+
+  def filteredMessageCount = filteredMessageCounter.get
+}
+
+class ForwarderBridgeServiceState[F[_]: Concurrent: Timer](bridge: ForwarderBridge[F, _, _]) extends ServiceState[F]
+  with CatsUtils with Logging {
+
+  def stop = bridge.stopped.set(true) >> pure(this)
+
+  def join: F[ServiceState[F]] = waitFor(bridge.stopped.get)(pure(_)) >> pure(this)
+
+  def service = bridge
 }
 
 object ForwarderBridge extends CatsUtils with Logging {
@@ -63,8 +78,11 @@ object ForwarderBridge extends CatsUtils with Logging {
   ) =
     for {
       stopped <- Ref.of[F, Boolean](false)
+      messageCounter <- Ref.of[F, Long](0)
+      errorCounter <- Ref.of[F, Long](0)
+      filteredMessageCounter <- Ref.of[F, Long](0)
     } yield
-      new ForwarderBridge[F, M, M](source, destination, filter, stopped)
+      new ForwarderBridge[F, M, M](source, destination, filter, stopped, messageCounter, errorCounter, filteredMessageCounter)
 
   def fixedDestination[F[_] : Concurrent, M](destination: SenderClient[F, M]) = { r: MessageReceiveResult[F, M] =>
     for {
