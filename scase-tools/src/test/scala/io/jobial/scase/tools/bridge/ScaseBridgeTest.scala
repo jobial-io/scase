17e4e2901e4cf4cfd73146f1d25588bee2b301e6
orbang <orbang@jobial.io>
2023-01-01 15:04:25 +0000

    Fixed bridge tests

--- scase-tools/src/test/scala/io/jobial/scase/tools/bridge/ScaseBridgeTest.scala
+++ scase-tools/src/test/scala/io/jobial/scase/tools/bridge/ScaseBridgeTest.scala
@@ -4,14 +4,11 @@ import cats.effect.IO
 import cats.effect.IO.pure
 import cats.kernel.Eq
 import io.circe.generic.auto._
-import io.jobial.scase.core.ReceiverClient
 import io.jobial.scase.core.RequestHandler
 import io.jobial.scase.core.RequestResponseClient
-import io.jobial.scase.core.SenderClient
 import io.jobial.scase.core.Service
 import io.jobial.scase.core.test.ServiceTestSupport
 import io.jobial.scase.core.test.TestRequest
-import io.jobial.scase.core.test.TestRequest1
 import io.jobial.scase.core.test.TestResponse
 import io.jobial.scase.jms.JMSServiceConfiguration
 import io.jobial.scase.pulsar.PulsarContext
@@ -20,7 +17,6 @@ import io.jobial.scase.tibrv.TibrvContext
 import io.jobial.scase.tibrv.TibrvServiceConfiguration
 import io.jobial.scase.tools.bridge.ScaseBridge._
 import io.jobial.scase.util.Hash.uuid
-import io.lemonlabs.uri.Uri
 import scala.concurrent.duration.DurationInt
 
 class ScaseBridgeTest extends ServiceTestSupport {
@@ -31,11 +27,11 @@ class ScaseBridgeTest extends ServiceTestSupport {
 
   implicit def parseEndpointInfo(v: String) = endpointInfoArgumentValueParser.parse(v).toOption.get
 
-//  "destination name pattern" should "work" in {
-//    assert("pulsar://host:port/tenant/namespace/XXX.DEV.YYY.ZZZ" == substituteDestinationName("pulsar://host:port/tenant/namespace/([A-Z].*)\\.PROD\\.(.*)", "pulsar://host:port/tenant/namespace/$1.DEV.$2", "pulsar://host:port/tenant/namespace/XXX.PROD.YYY.ZZZ"))
-//    assert("pulsar://host:port/tenant/namespace/XXX.DEV.XXX.YYY.ZZZ" == substituteDestinationName("tibrv://host:port/network/service/([A-Z].*)\\.PROD\\.(.*)", "pulsar://host:port/tenant/namespace/$1.DEV.$2", "tibrv://host:port/network/service/XXX.PROD.XXX.YYY.ZZZ"))
-//    assert("pulsar://host:port/tenant/namespace/XXX.DEV.XXX.YYY.ZZZ" == substituteDestinationName("tibrv://host:port/network/service/([A-Z].*)\\.PROD\\.(.*).>", "pulsar://host:port/tenant/namespace/$1.DEV.$2", "tibrv://host:port/network/service/XXX.PROD.XXX.YYY.ZZZ"))
-//  }
+  //  "destination name pattern" should "work" in {
+  //    assert("pulsar://host:port/tenant/namespace/XXX.DEV.YYY.ZZZ" == substituteDestinationName("pulsar://host:port/tenant/namespace/([A-Z].*)\\.PROD\\.(.*)", "pulsar://host:port/tenant/namespace/$1.DEV.$2", "pulsar://host:port/tenant/namespace/XXX.PROD.YYY.ZZZ"))
+  //    assert("pulsar://host:port/tenant/namespace/XXX.DEV.XXX.YYY.ZZZ" == substituteDestinationName("tibrv://host:port/network/service/([A-Z].*)\\.PROD\\.(.*)", "pulsar://host:port/tenant/namespace/$1.DEV.$2", "tibrv://host:port/network/service/XXX.PROD.XXX.YYY.ZZZ"))
+  //    assert("pulsar://host:port/tenant/namespace/XXX.DEV.XXX.YYY.ZZZ" == substituteDestinationName("tibrv://host:port/network/service/([A-Z].*)\\.PROD\\.(.*).>", "pulsar://host:port/tenant/namespace/$1.DEV.$2", "tibrv://host:port/network/service/XXX.PROD.XXX.YYY.ZZZ"))
+  //  }
 
   "pulsar to pulsar" should "work" in {
     val topic = s"hello-test-${uuid(6)}"
@@ -65,8 +61,8 @@ class ScaseBridgeTest extends ServiceTestSupport {
     for {
       context <- BridgeContext(s"pulsar://///${topic}", "tibrv://", false, 300.seconds)
       r <- {
+        implicit val pulsarContext = context.source.asInstanceOf[PulsarEndpointInfo].context
         implicit val tibrvContext = context.destination.asInstanceOf[TibrvEndpointInfo].context
-        implicit val pulsarContext = context.destination.asInstanceOf[PulsarEndpointInfo].context
         testRequestResponseBridge(
           TibrvServiceConfiguration.requestResponse[TestRequest[_ <: TestResponse], TestResponse](Seq(topic)).service(_),
           PulsarServiceConfiguration.requestResponse[TestRequest[_ <: TestResponse], TestResponse](topic).client,
@@ -76,21 +72,26 @@ class ScaseBridgeTest extends ServiceTestSupport {
     } yield r
   }
 
-  //  "rv to pulsar" should "work" in {
-  //    assume(!onMacOS)
-  //
-  //    val topic = s"hello-test-${uuid(6)}"
-  //    import io.jobial.scase.marshalling.tibrv.circe._
-  //    import io.jobial.scase.marshalling.tibrv.raw.tibrvMsgRawMarshalling
-  //
-  //    testBridge(
-  //      PulsarServiceConfiguration.requestResponse[TestRequest[_ <: TestResponse], TestResponse](topic).service(_),
-  //      TibrvServiceConfiguration.requestResponse[TestRequest[_ <: TestResponse], TestResponse](Seq(topic)).client,
-  //      PulsarServiceConfiguration.source[TestRequest1](topic).client,
-  //      TibrvServiceConfiguration.destination[TestRequest1](topic).client,
-  //      BridgeContext(s"tibrv://${topic}", "pulsar://", sourceTibrvContext = Some(tibrvContext), sourcePulsarContext = Some(pulsarContext))
-  //    )
-  //  }
+  "rv to pulsar" should "work" in {
+    assume(!onMacOS)
+
+    val topic = s"hello-test-${uuid(6)}"
+    import io.jobial.scase.marshalling.tibrv.circe._
+    import io.jobial.scase.marshalling.tibrv.raw.tibrvMsgRawMarshalling
+
+    for {
+      context <- BridgeContext(s"tibrv://///${topic}", "pulsar://", false, 300.seconds)
+      r <- {
+        implicit val tibrvContext = context.source.asInstanceOf[TibrvEndpointInfo].context
+        implicit val pulsarContext = context.destination.asInstanceOf[PulsarEndpointInfo].context
+        testRequestResponseBridge(
+          PulsarServiceConfiguration.requestResponse[TestRequest[_ <: TestResponse], TestResponse](topic).service(_),
+          TibrvServiceConfiguration.requestResponse[TestRequest[_ <: TestResponse], TestResponse](Seq(topic)).client,
+          pure(context)
+        )
+      }
+    } yield r
+  }
 
   "pulsar to jms" should "work" in {
     val topic = s"hello-test-${uuid(6)}"
@@ -128,12 +129,12 @@ class ScaseBridgeTest extends ServiceTestSupport {
   //      BridgeContext[Any](pulsarContext = Some(pulsarContext), activemqContext = Some(activemqContext))
   //    )
   //  }
-  
+
   def testRequestResponseBridge[M](
     destinationService: RequestHandler[IO, TestRequest[_ <: TestResponse], TestResponse] => IO[Service[IO]],
     sourceClient: IO[RequestResponseClient[IO, TestRequest[_ <: TestResponse], TestResponse]],
-//    destinationReceiverClient: IO[ReceiverClient[IO, TestRequest1]],
-//    sourceSenderClient: IO[SenderClient[IO, TestRequest1]],
+    //    destinationReceiverClient: IO[ReceiverClient[IO, TestRequest1]],
+    //    sourceSenderClient: IO[SenderClient[IO, TestRequest1]],
     bridgeContext: IO[BridgeContext[M]]
   ) =
     for {
