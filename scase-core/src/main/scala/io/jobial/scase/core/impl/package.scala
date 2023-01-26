df29385770c916e103919c8ff7c16f5426ed9e9f
orbang <orbang@jobial.io>
2023-01-26 18:04:09 +0000

    Create daemon threads in blocker pool

--- scase-core/src/main/scala/io/jobial/scase/core/impl/package.scala
+++ scase-core/src/main/scala/io/jobial/scase/core/impl/package.scala
@@ -1,9 +1,18 @@
 package io.jobial.scase.core
 
 import java.util.concurrent.Executors.newCachedThreadPool
+import java.util.concurrent.ThreadFactory
 import scala.concurrent.ExecutionContext.fromExecutor
 
 package object impl {
 
-  val blockerContext = fromExecutor(newCachedThreadPool)
+  object DaemonThreadFactory extends ThreadFactory {
+    def newThread(r: Runnable) = {
+      val t = new Thread(r)
+      t.setDaemon(true)
+      t
+    }
+  }
+
+  val blockerContext = fromExecutor(newCachedThreadPool(DaemonThreadFactory))
 }
