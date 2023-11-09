2bf2d73b2e1d4c57be96a594b8bfed8cb99008c6
orbang <orbang@jobial.io>
2023-11-09 23:42:38 +0000

    Improved ECS support

--- scase-aws/src/main/scala/io/jobial/scase/aws/client/ECSClient.scala
+++ scase-aws/src/main/scala/io/jobial/scase/aws/client/ECSClient.scala
@@ -11,6 +11,9 @@ import com.amazonaws.services.ecs.model.DescribeTasksRequest
 import com.amazonaws.services.ecs.model.ListClustersRequest
 import com.amazonaws.services.ecs.model.ListContainerInstancesRequest
 import com.amazonaws.services.ecs.model.ListServicesRequest
+import com.amazonaws.services.ecs.model.ListTagsForResourceRequest
+import com.amazonaws.services.ecs.model.ListTagsForResourceResult
+import com.amazonaws.services.ecs.model.ListTagsForResourceResult
 import com.amazonaws.services.ecs.model.ListTasksRequest
 import com.amazonaws.services.ecs.model.Service
 import com.amazonaws.services.ecs.model.Task
@@ -36,10 +39,6 @@ trait ECSClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
       clusters <- describeClusters(clusters.getClusterArns.asScala.toList)
     } yield clusters.getClusters.asScala.toList
   
-  implicit val clusterTagged = new Tagged[Cluster] {
-    def tags(tagged: Cluster) = tagged.getTags.asScala.toList.map(t => Tag(t.getKey, t.getValue))
-  }
-
   def listServices(clusterId: String)(implicit context: AwsContext, concurrent: Concurrent[F]) =
     fromJavaFuture(context.ecs.listServicesAsync(
       new ListServicesRequest().withCluster(clusterId)
@@ -55,10 +54,6 @@ trait ECSClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
       services <- listServices(clusterId)
       services <- describeServices(clusterId, services.getServiceArns.asScala.toList)
     } yield services.getServices.asScala.toList
-
-  implicit val serviceTagged = new Tagged[Service] {
-    def tags(tagged: Service) = tagged.getTags.asScala.toList.map(t => Tag(t.getKey, t.getValue))
-  }
 
   def listTasks(clusterId: String)(implicit context: AwsContext, concurrent: Concurrent[F]) =
     fromJavaFuture(context.ecs.listTasksAsync(
@@ -76,10 +71,6 @@ trait ECSClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
       tasks <- describeTasks(clusterId, tasks.getTaskArns.asScala.toList)
     } yield tasks.getTasks.asScala.toList
 
-  implicit val taskTagged = new Tagged[Task] {
-    def tags(tagged: Task) = tagged.getTags.asScala.toList.map(t => Tag(t.getKey, t.getValue))
-  }
-
   def listContainerInstances(implicit context: AwsContext, concurrent: Concurrent[F]) =
     fromJavaFuture(context.ecs.listContainerInstancesAsync(
       new ListContainerInstancesRequest()
@@ -90,8 +81,32 @@ trait ECSClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
       new DescribeContainerInstancesRequest()
     ))
 
-  implicit val containerInstanceTagged = new Tagged[ContainerInstance] {
-    def tags(tagged: ContainerInstance) = tagged.getTags.asScala.toList.map(t => Tag(t.getKey, t.getValue))
+  def listContainerInstances(clusterId: String)(implicit context: AwsContext, concurrent: Concurrent[F]) =
+    fromJavaFuture(context.ecs.listContainerInstancesAsync(
+      new ListContainerInstancesRequest()
+        .withCluster(clusterId)
+    ))
+
+  def describeContainerInstances(clusterId: String, containerInstances: List[String])(implicit context: AwsContext, concurrent: Concurrent[F]) =
+    fromJavaFuture(context.ecs.describeContainerInstancesAsync(
+      new DescribeContainerInstancesRequest()
+        .withCluster(clusterId)
+        .withContainerInstances(containerInstances.asJava)
+    ))
+
+  def describeAllContainerInstances(clusterId: String)(implicit awsContext: AwsContext, concurrent: Concurrent[F]) =
+    for {
+      containerInstances <- listContainerInstances(clusterId)
+      containerInstances <- describeContainerInstances(clusterId, containerInstances.getContainerInstanceArns.asScala.toList)
+    } yield containerInstances.getContainerInstances.asScala.toList
+
+  def listTagsForResource(resourceArn: String)(implicit context: AwsContext, concurrent: Concurrent[F]) =
+    fromJavaFuture(context.ecs.listTagsForResourceAsync(
+      new ListTagsForResourceRequest().withResourceArn(resourceArn)
+    ))
+
+  implicit val listTagsForResourceResultTagged = new Tagged[ListTagsForResourceResult] {
+    def tags(tagged: ListTagsForResourceResult) = tagged.getTags.asScala.toList.map(t => Tag(t.getKey, t.getValue))
   }
 }
 
