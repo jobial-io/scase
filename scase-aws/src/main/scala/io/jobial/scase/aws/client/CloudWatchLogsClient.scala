aaabaf0b018e3dcd5cbb811108fb8467970caa4b
orbang <orbang@jobial.io>
2023-11-16 00:47:57 +0000

    Improved CloudWatch logs support

--- scase-aws/src/main/scala/io/jobial/scase/aws/client/CloudWatchLogsClient.scala
+++ scase-aws/src/main/scala/io/jobial/scase/aws/client/CloudWatchLogsClient.scala
@@ -2,19 +2,24 @@ package io.jobial.scase.aws.client
 
 import cats.effect.Concurrent
 import cats.implicits._
+import com.amazonaws.services.logs.model.DescribeLogGroupsRequest
 import com.amazonaws.services.logs.model.DescribeLogStreamsRequest
+import com.amazonaws.services.logs.model.OrderBy
 import io.jobial.sprint.util.CatsUtils
 
 import scala.collection.JavaConverters._
 
 trait CloudWatchLogsClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
   
-  def describeLogGroups(implicit awsContext: AwsContext, concurrent: Concurrent[F]) =
-    fromJavaFuture(awsContext.logs.describeLogGroupsAsync()).map(_.getLogGroups.asScala.toList)
+  def describeLogGroups(limit: Int = 10000)(implicit awsContext: AwsContext, concurrent: Concurrent[F]) =
+    fromJavaFuture(awsContext.logs.describeLogGroupsAsync(
+      new DescribeLogGroupsRequest().withLimit(limit)
+    )).map(_.getLogGroups.asScala.toList)
 
-  def describeLogStreams(logGroup: String)(implicit awsContext: AwsContext, concurrent: Concurrent[F]) =
+  def describeLogStreams(logGroup: String, limit: Int = 10)(implicit awsContext: AwsContext, concurrent: Concurrent[F]) =
     fromJavaFuture(awsContext.logs.describeLogStreamsAsync(
-      new DescribeLogStreamsRequest().withLogGroupName(logGroup)
+      new DescribeLogStreamsRequest().withLogGroupName(logGroup).withLimit(limit)
+        .withOrderBy(OrderBy.LastEventTime).withDescending(true)
     )).map(_.getLogStreams.asScala.toList)
 }  
 
