fcd53551892e02b15fffe056b9f18090ad15305c
orbang <orbang@jobial.io>
2023-11-16 02:05:27 +0000

    Fixed AWS logs pagination

--- scase-aws/src/main/scala/io/jobial/scase/aws/client/CloudWatchLogsClient.scala
+++ scase-aws/src/main/scala/io/jobial/scase/aws/client/CloudWatchLogsClient.scala
@@ -4,22 +4,50 @@ import cats.effect.Concurrent
 import cats.implicits._
 import com.amazonaws.services.logs.model.DescribeLogGroupsRequest
 import com.amazonaws.services.logs.model.DescribeLogStreamsRequest
+import com.amazonaws.services.logs.model.LogGroup
+import com.amazonaws.services.logs.model.LogStream
 import com.amazonaws.services.logs.model.OrderBy
 import io.jobial.sprint.util.CatsUtils
 
 import scala.collection.JavaConverters._
 
 trait CloudWatchLogsClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
-  
-  def describeLogGroups(limit: Int = 10000)(implicit awsContext: AwsContext, concurrent: Concurrent[F]) =
-    fromJavaFuture(awsContext.logs.describeLogGroupsAsync(
-      new DescribeLogGroupsRequest().withLimit(limit)
-    )).map(_.getLogGroups.asScala.toList)
 
-  def describeLogStreams(logGroup: String, limit: Int = 10)(implicit awsContext: AwsContext, concurrent: Concurrent[F]) =
-    fromJavaFuture(awsContext.logs.describeLogStreamsAsync(
-      new DescribeLogStreamsRequest().withLogGroupName(logGroup).withLimit(limit)
-        .withOrderBy(OrderBy.LastEventTime).withDescending(true)
-    )).map(_.getLogStreams.asScala.toList)
+  def describeLogGroups(limit: Int = 1000, nextToken: Option[String] = None)(implicit awsContext: AwsContext, concurrent: Concurrent[F]): F[List[LogGroup]] =
+    for {
+      r <- fromJavaFuture(awsContext.logs.describeLogGroupsAsync {
+        val request = new DescribeLogGroupsRequest()
+        nextToken.map(request.withNextToken).getOrElse(request)
+      })
+      rest <- Option(r.getNextToken) match {
+        case Some(token) =>
+          val remaining = limit - r.getLogGroups.size
+          if (remaining > 0)
+            describeLogGroups(remaining, Some(token))
+          else
+            pure(List())
+        case None =>
+          pure(List())
+      }
+    } yield r.getLogGroups.asScala.toList ++ rest
+
+  def describeLogStreams(logGroup: String, limit: Int = 1000, nextToken: Option[String] = None)(implicit awsContext: AwsContext, concurrent: Concurrent[F]): F[List[LogStream]] =
+    for {
+      r <- fromJavaFuture(awsContext.logs.describeLogStreamsAsync {
+        val request = new DescribeLogStreamsRequest().withLogGroupName(logGroup)
+          .withOrderBy(OrderBy.LastEventTime).withDescending(true)
+        nextToken.map(request.withNextToken).getOrElse(request)
+      })
+      rest <- Option(r.getNextToken) match {
+        case Some(token) =>
+          val remaining = limit - r.getLogStreams.size
+          if (remaining > 0)
+            describeLogStreams(logGroup, remaining, Some(token))
+          else
+            pure(List())
+        case None =>
+          pure(List())
+      }
+    } yield r.getLogStreams.asScala.toList ++ rest
 }  
 
