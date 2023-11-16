7eb54fb52b2a5a5e19c5d6fc1a71314c959628d3
orbang <orbang@jobial.io>
2023-11-16 22:57:41 +0000

    Added filter log events support

--- scase-aws/src/main/scala/io/jobial/scase/aws/client/CloudWatchLogsClient.scala
+++ scase-aws/src/main/scala/io/jobial/scase/aws/client/CloudWatchLogsClient.scala
@@ -4,6 +4,8 @@ import cats.effect.Concurrent
 import cats.implicits._
 import com.amazonaws.services.logs.model.DescribeLogGroupsRequest
 import com.amazonaws.services.logs.model.DescribeLogStreamsRequest
+import com.amazonaws.services.logs.model.FilterLogEventsRequest
+import com.amazonaws.services.logs.model.FilteredLogEvent
 import com.amazonaws.services.logs.model.GetLogEventsRequest
 import com.amazonaws.services.logs.model.LogGroup
 import com.amazonaws.services.logs.model.LogStream
@@ -52,12 +54,35 @@ trait CloudWatchLogsClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
       }
     } yield r.getLogStreams.asScala.take(limit).toList ++ rest
     
-  def getLogEvents(logGroup: String, logStream: String, startTime: Long, endTime: Long)(implicit awsContext: AwsContext, concurrent: Concurrent[F]): F[List[OutputLogEvent]] =
+  def getLogEvents(logGroup: String, logStream: String, startTime: Long, endTime: Long)(implicit awsContext: AwsContext, concurrent: Concurrent[F]): F[Vector[OutputLogEvent]] =
     fromJavaFuture(awsContext.logs.getLogEventsAsync {
       new GetLogEventsRequest().withLogGroupName(logGroup)
         .withLogStreamName(logStream)
         .withStartTime(startTime)
         .withEndTime(endTime)
-    }).map(_.getEvents.asScala.toList)
+    }).map(_.getEvents.asScala.toVector)
+
+  def filterLogEvents(logGroup: String, startTime: Long, endTime: Long, filterPattern: Option[String], limit: Int = 10000, nextToken: Option[String] = None)(implicit awsContext: AwsContext, concurrent: Concurrent[F]): F[List[FilteredLogEvent]] =
+    for {
+      r <- fromJavaFuture(awsContext.logs.filterLogEventsAsync {
+        val request = new FilterLogEventsRequest()
+          .withLogGroupName(logGroup)
+          .withStartTime(startTime)
+          .withEndTime(endTime)
+        val request1 = filterPattern.map(request.withFilterPattern).getOrElse(request)
+        nextToken.map(request1.withNextToken).getOrElse(request1)
+      })
+      rest <- Option(r.getNextToken) match {
+        case Some(token) =>
+          val remaining = limit - r.getEvents.size
+          if (remaining > 0)
+            filterLogEvents(logGroup, startTime, endTime, filterPattern, remaining, Some(token))
+          else
+            pure(List())
+        case None =>
+          pure(List())
+      }
+    } yield r.getEvents.asScala.take(limit).toList ++ rest
+
 }  
 
