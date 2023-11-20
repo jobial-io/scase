0f27f19aea9163a9136cdc2cf76f3795f48ae65c
orbang <orbang@jobial.io>
2023-11-20 07:50:47 +0000

    Added support for deploying apps as lambda

---
+++ scase-aws/src/main/scala/io/jobial/scase/aws/lambda/AppLambda.scala
@@ -0,0 +1,23 @@
+package io.jobial.scase.aws.lambda
+
+import cats.effect.IO
+import cats.implicits.catsSyntaxFlatMapOps
+import io.jobial.scase.core.RequestContext
+import io.jobial.scase.core.RequestResponseMapping
+import io.jobial.scase.marshalling.circe._
+import io.jobial.scase.core._
+
+abstract class AppLambda[C <: {def main(args: Array[String])}](app: C) extends IOLambdaRequestHandler[Array[String], Unit] {
+
+  implicit val mapping = new RequestResponseMapping[Array[String], Unit] {}
+
+  def functionName = getClass.getSimpleName
+
+  override def serviceConfiguration = LambdaServiceConfiguration.requestResponse[Array[String], Unit](functionName)
+
+  override def handleRequest(implicit context: RequestContext[IO]) = {
+    case args =>
+      delay(app.main(args)) >>
+        args.reply(())
+  }
+}
