2a2ed298f1e527f29735783c4dc2d2793c340ee2
orbang <orbang@jobial.io>
2023-11-27 12:26:24 +0000

    Added resource record delete support

--- scase-aws/src/main/scala/io/jobial/scase/aws/client/Route53Client.scala
+++ scase-aws/src/main/scala/io/jobial/scase/aws/client/Route53Client.scala
@@ -2,9 +2,14 @@ package io.jobial.scase.aws.client
 
 import cats.effect.Concurrent
 import cats.implicits._
+import com.amazonaws.services.route53.model.Change
+import com.amazonaws.services.route53.model.ChangeAction
+import com.amazonaws.services.route53.model.ChangeBatch
+import com.amazonaws.services.route53.model.ChangeResourceRecordSetsRequest
 import com.amazonaws.services.route53.model.GetHostedZoneRequest
 import com.amazonaws.services.route53.model.ListHostedZonesRequest
 import com.amazonaws.services.route53.model.ListResourceRecordSetsRequest
+import com.amazonaws.services.route53.model.ResourceRecordSet
 import io.jobial.sprint.util.CatsUtils
 
 import scala.collection.JavaConverters._
@@ -32,5 +37,27 @@ trait Route53Client[F[_]] extends AwsClient[F] with CatsUtils[F] {
       records <- hostedZones.map { z => listResourceRecordSets(z.getId) }.sequence.map(_.flatten)
     } yield records.filter(_.getType === "A").find(_.getName === name)
       .flatMap(_.getResourceRecords.asScala.headOption.map(_.getValue))
+
+  def changeResourceRecordSets(request: ChangeResourceRecordSetsRequest)(implicit context: AwsContext, concurrent: Concurrent[F]) =
+    fromJavaFuture(context.route53.changeResourceRecordSetsAsync(
+      request
+    ))
+
+  def deleteRecord(hostedZoneId: String, name: String)(implicit context: AwsContext, concurrent: Concurrent[F]) =
+    changeResourceRecordSets(
+      new ChangeResourceRecordSetsRequest()
+        .withHostedZoneId(hostedZoneId)
+        .withChangeBatch(
+          new ChangeBatch()
+            .withChanges(
+              new Change()
+                .withAction(ChangeAction.DELETE)
+                .withResourceRecordSet(
+                  new ResourceRecordSet()
+                    .withName(name)
+                )
+            )
+        )
+    )
 }  
 
