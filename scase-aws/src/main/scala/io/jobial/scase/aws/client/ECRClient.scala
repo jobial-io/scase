b4b875ec9abc7ab512d528615ac91011d682574a
orbang <orbang@jobial.io>
2024-01-05 21:34:41 +0000

    Renaming

--- scase-aws/src/main/scala/io/jobial/scase/aws/client/ECRClient.scala
+++ scase-aws/src/main/scala/io/jobial/scase/aws/client/ECRClient.scala
@@ -11,7 +11,7 @@ import io.jobial.sprint.util.CatsUtils
 
 import scala.collection.JavaConverters._
 
-trait EcrClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
+trait ECRClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
 
   def describeRepositories(implicit context: AwsContext, concurrent: Concurrent[F]) =
     fromJavaFuture(context.ecr.describeRepositoriesAsync(
