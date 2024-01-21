3e738c5fbf910643787d3d9ab99aa6699b52ffd1
orbang <orbang@jobial.io>
2024-01-21 06:25:17 +0000

    Added tagging for DB clusters

--- scase-aws/src/main/scala/io/jobial/scase/aws/client/RDSClient.scala
+++ scase-aws/src/main/scala/io/jobial/scase/aws/client/RDSClient.scala
@@ -2,6 +2,7 @@ package io.jobial.scase.aws.client
 
 import cats.effect.Concurrent
 import cats.implicits._
+import com.amazonaws.services.rds.model.DBCluster
 import com.amazonaws.services.rds.model.DescribeDBClustersRequest
 import io.jobial.sprint.util.CatsUtils
 
@@ -9,9 +10,13 @@ import scala.collection.JavaConverters._
 
 trait RDSClient[F[_]] extends AwsClient[F] with CatsUtils[F] {
 
-  def describeRepositories(implicit context: AwsContext, concurrent: Concurrent[F]) =
+  def describeDBClusters(implicit context: AwsContext, concurrent: Concurrent[F]) =
     fromJavaFuture(context.rds.describeDBClustersAsync(
       new DescribeDBClustersRequest
     )).map(_.getDBClusters.asScala.toList)
+
+  implicit val clusterTagged = new Tagged[DBCluster] {
+    def tags(tagged: DBCluster) = tagged.getTagList.asScala.toList.map(t => Tag(t.getKey, t.getValue))
+  }
 }
 
